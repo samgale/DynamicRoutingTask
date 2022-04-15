3e0fc8ab740147f5952421ccb134ccecf34ab1bb
Sam Gale <samueldgale@gmail.com>
2022-04-15 13:29:51 -0700

    Update sound_sync_test.py

--- Analysis/sound_sync_test.py
+++ Analysis/sound_sync_test.py
@@ -5,6 +5,7 @@ Created on Thu Apr 14 10:51:54 2022
 @author: svc_ccg
 """
 
+import os
 import numpy as np
 import matplotlib.pyplot as plt
 import h5py
@@ -14,7 +15,7 @@ import ecephys
 
 
 # sync
-syncPath = r"\\allen\programs\mindscope\workgroups\dynamicrouting\Pilot ephys\sound_sync_test_04122022_1\20220414T173844.h5"
+syncPath = r"\\allen\programs\mindscope\workgroups\dynamicrouting\Pilot ephys\sound_sync_test_04152021_1\20220415T13643.h5"
 
 syncDataset = sync.Dataset(syncPath)
     
@@ -26,8 +27,8 @@ syncBarcodeRising,syncBarcodeFalling = probeSync.get_sync_line_data(syncDataset,
 syncBarcodeTimes,syncBarcodes = ecephys.extract_barcodes_from_times(syncBarcodeRising,syncBarcodeFalling)
 
 # ephys
-datPath = r"\\allen\programs\mindscope\workgroups\dynamicrouting\Pilot ephys\sound_sync_test_04122022_1\2022-04-14_17-38-47\Record Node 105\experiment1\recording1\continuous\NI-DAQmx-103.0\continuous.dat"
-datTimestampsPath = r"\\allen\programs\mindscope\workgroups\dynamicrouting\Pilot ephys\sound_sync_test_04122022_1\2022-04-14_17-38-47\Record Node 105\experiment1\recording1\continuous\NI-DAQmx-103.0\timestamps.npy"
+datPath = r"\\allen\programs\mindscope\workgroups\dynamicrouting\Pilot ephys\sound_sync_test_04152021_1\2022-04-15_13-06-47\Record Node 105\experiment1\recording1\continuous\NI-DAQmx-103.0\continuous.dat"
+datTimestampsPath = os.path.join(os.path.dirname(datPath),'timestamps.npy')
 
 sampleRate = 30000
 
@@ -42,8 +43,8 @@ microphoneCh = 3
 speakerData = datData[speakerCh]
 microphoneData = datData[microphoneCh]
 
-ttlStatesPath = r"\\allen\programs\mindscope\workgroups\dynamicrouting\Pilot ephys\sound_sync_test_04122022_1\2022-04-14_17-38-47\Record Node 105\experiment1\recording1\events\NI-DAQmx-103.0\TTL_1\channel_states.npy"
-ttlTimestampsPath = r"\\allen\programs\mindscope\workgroups\dynamicrouting\Pilot ephys\sound_sync_test_04122022_1\2022-04-14_17-38-47\Record Node 105\experiment1\recording1\events\NI-DAQmx-103.0\TTL_1\timestamps.npy"
+ttlStatesPath = r"\\allen\programs\mindscope\workgroups\dynamicrouting\Pilot ephys\sound_sync_test_04152021_1\2022-04-15_13-06-47\Record Node 105\experiment1\recording1\events\NI-DAQmx-103.0\TTL_1\channel_states.npy"
+ttlTimestampsPath = os.path.join(os.path.dirname(ttlStatesPath),'timestamps.npy')
 
 ttlStates = np.load(ttlStatesPath)
 ttlTimestamps = np.load(ttlTimestampsPath) - datTimestamps[0]
@@ -56,7 +57,7 @@ shift,relSampleRate,endpoints = ecephys.get_probe_time_offset(syncBarcodeTimes,s
 
 
 # behavior/stimuli
-behavPath = r"\\allen\programs\mindscope\workgroups\dynamicrouting\Pilot ephys\sound_sync_test_04122022_1\DynamicRouting1_test_20220414_173855.hdf5"
+behavPath = r"\\allen\programs\mindscope\workgroups\dynamicrouting\Pilot ephys\sound_sync_test_04152021_1\DynamicRouting1_test_20220415_130656.hdf5"
 
 d = h5py.File(behavPath,'r')
 
