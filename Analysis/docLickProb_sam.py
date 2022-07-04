5eca0dac5001f940a40f4a9b8015bbbb87c2a661
Sam Gale <samueldgale@gmail.com>
2022-07-03 23:57:41 -0700

    Add files via upload

--- Analysis/docLickProb_sam.py
+++ Analysis/docLickProb_sam.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 """
 Created on Thu Jun 30 15:27:03 2022
-
 @author: svc_ccg
 """
 
@@ -12,27 +11,21 @@ import matplotlib.pyplot as plt
     
 class DocSim():
     
-    def __init__(self,nTrials,lickProbChange=0,lickProbTiming=[0],timingProb=0):
-        self.nTrials = nTrials
+    def __init__(self,lickProb=0,lickProbChange=0,lickProbTiming=[0],timingProb=0):
+        self.lickProb = lickProb
         self.lickProbChange = lickProbChange
         self.lickProbTiming = lickProbTiming
         self.timingProb = timingProb
+        self.flashInterval = 0.75 # seconds
         self.catchProb = 0.125
         self.timeoutFlashes = 0
         self.graceFlashes = 5
         self.maxAborts = 5
     
-    def pickChangeFlash(self,p=0.3,nmin=5,nmax=12):
-        n = np.random.geometric(p)
-        if nmin <= n <= nmax:
-            return n
-        else:
-            return self.pickChangeFlash()
-    
     def runTrial(self):
         self.trialStartFlash.append(self.flash+1)
         if self.aborts <= self.maxAborts:
-            self.trialChangeFlash = self.pickChangeFlash()
+            self.trialChangeFlash = pickChangeFlash()
         lick = False
         outcome = False
         for trialFlash in range(1,self.trialChangeFlash+1):
@@ -40,10 +33,8 @@ class DocSim():
             if trialFlash == self.trialChangeFlash:
                 if random.random() < self.catchProb:
                     isCatch,isChange = True,False
-                    self.catchFlash.append(self.flash)
                 else:
                     isCatch,isChange = False,True
-                    self.changeFlash.append(self.flash)
             else:
                 isCatch = isChange = False
             lick = False
@@ -52,6 +43,8 @@ class DocSim():
                     lick = True
             elif isChange and random.random() < self.lickProbChange:
                 lick = True
+            elif random.random() < self.lickProb:
+                lick = True
             if isChange:
                 outcome = 'hit' if lick else 'miss'
             elif isCatch:
@@ -59,6 +52,8 @@ class DocSim():
             elif lick:
                 outcome = 'abort'
             if outcome:
+                self.trialOutcomeFlash.append(self.flash)
+                self.trialOutcome.append(outcome)
                 if outcome == 'abort':
                     if self.aborts < self.maxAborts:
                         self.aborts += 1
@@ -68,28 +63,126 @@ class DocSim():
                 else:
                     self.aborts = 0
                     self.flash += self.graceFlashes
-                self.trialOutcome.append(outcome)
                 break
                 
-    def runSession(self):
+    def runSession(self,sessionHours):
         self.flash = 0
-        self.trialStartFlash = []
-        self.changeFlash = []
-        self.catchFlash = []
-        self.trialOutcome = []
         self.aborts = 0
-        for _ in range(self.nTrials):
+        self.trialStartFlash = []
+        self.trialOutcomeFlash = []
+        self.trialOutcome = []
+        self.rewardRate = []
+        hours = 0
+        while hours < sessionHours:
             self.runTrial()
-        
-
-
-
+            hours = self.flash * self.flashInterval / 3600
+            self.rewardRate.append(sum(outcome=='hit' for outcome in self.trialOutcome)/hours)
+            
+            
+def pickChangeFlash(p=0.3,nmin=5,nmax=12):
+    n = np.random.geometric(p)
+    if nmin <= n <= nmax:
+        return n
+    else:
+        return pickChangeFlash()
     
 
-# n = np.array([pickChangeFlash(p,nmin,nmax) for _ in range(100000)])
-# flashNum = np.arange(1,n.max()+1)
-# changeProb = np.array([np.sum(n==i)/(n.size/(1-catchProb)) for i in flashNum])
-# conditionalChangeProb = np.array([changeProb[i]/(changeProb[i:].sum()+catchProb) for i in range(n.max())])
+# calculate change prob
+catchProb = 0.125    
+n = np.array([pickChangeFlash() for _ in range(100000)])
+flashNum = np.arange(1,n.max()+1)
+changeProb = np.array([np.sum(n==i)/n.size for i in flashNum]) * (1-catchProb)
+conditionalChangeProb = np.array([changeProb[i]/(changeProb[i:].sum()/(1-catchProb)) for i in range(n.max())])
+
+fig = plt.figure()
+ax = fig.add_subplot(1,1,1)
+ax.plot(flashNum,changeProb,'ko-',label='change prob.')
+ax.plot(flashNum,conditionalChangeProb,'bo-',label='change prob. given no change previous flash')
+for side in ('right','top'):
+    ax.spines[side].set_visible(False)
+ax.tick_params(direction='out',top=False,right=False)
+ax.set_ylim([0,1])
+ax.set_xlabel('flash number')
+ax.set_ylabel('probability')
+ax.legend(loc='upper left',fontsize=8)
+plt.tight_layout()
+
+
+#
+doc = DocSim(lickProb=0,lickProbChange=1)
+doc.runSession(sessionHours=10)
+
+
+
+p = np.arange(0,1.05,0.1)
+rewardRate = np.zeros((p.size,)*2)
+for i,lickProb in enumerate(p):
+    for j,lickProbChange in enumerate(p):
+        doc = DocSim(lickProb,lickProbChange)
+        doc.runSession(sessionHours=10)
+        rewardRate[i,j] = doc.rewardRate[-1]
+        
+plt.imshow(rewardRate)
+
+            
+            
+            
+def gamma(x,alpha,tau,eta):
+    return ((alpha*(x-tau))**eta * np.exp(-alpha*(x-tau))) / (eta**eta * np.exp(-eta))
+
+
+x = np.arange(1,13)
+alpha = [1]
+tau = [2,4,8]
+eta = [1,2,4]
+
+
+fig = plt.figure()
+ax = fig.add_subplot(1,1,1)
+for a in alpha:
+    for t in tau:
+        for e in eta:
+            y = gamma(x,a,t,e)
+            ax.plot(x,y,label=(a,t,e))
+ax.set_ylim([0,1])
+ax.legend(fontsize=6)
+
+
+import scipy.stats
+
+fig = plt.figure()
+ax = fig.add_subplot(1,1,1)
+for a in [6]:
+    for s in [0]:
+        y = scipy.stats.gamma.pdf(x,a,scale=s)
+        ax.plot(x,y/y.max(),label=(a,s))
+ax.legend(fontsize=6)
+
+
+        
+
+doc = DocSim()
+
+doc.runSession(sessionHours=100)
+
+
+isChange = ~np.isnan(doc.changeFlash)
+isCatch = ~np.isnan(doc.catchFlash)
+isAbort = doc.trialOutcome == 'abort'
+
+flashesToChange = np.array(doc.changeFlash)[isChange] - np.array(doc.trialStartFlash)[isChange] + 1
+flashesToCatch = np.array(doc.catchFlash)[isCatch] - np.array(doc.trialStartFlash)[isCatch] + 1
+flashesToAbort = np.array(doc.)
+
+flashNum = np.arange(1,flashesToChange.max()+1)
+
+changeProb = np.array([(np.sum(flashesToChange==n) - np.sum(flashesToCatch==n)) / (flashesToChange.size + flashesToCatch.size) for n in flashNum])
+
+changeProb = np.array([np.sum(flashesToChange==n)/(flashesToChange.size/(1-doc.catchProb)) for n in flashNum])
+
+
+
+
 
 
 n = np.array([getChangeFlash() for _ in range(100000)])
@@ -122,7 +215,6 @@ ax.set_xlabel('flash number')
 ax.set_ylabel('probability')
 ax.legend(bbox_to_anchor=(0.5,1),fontsize=8)
 plt.tight_layout()
-
 
 
 
