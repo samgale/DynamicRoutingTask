6db6c98319bc4086e20296be6bec0c26f222ce9e
Sam Gale <samueldgale@gmail.com>
2023-03-10 19:17:25 -0800

    sdg

--- OptoGui/OptoGui.py
+++ OptoGui/OptoGui.py
@@ -8,6 +8,8 @@ GUI for initiating camstim or samstim scripts
 from __future__ import division
 import os
 import subprocess
+import numpy as np
+import pandas as pd
 from PyQt5 import QtCore, QtWidgets
 
 
@@ -24,6 +26,7 @@ class OptoGui():
     def __init__(self,app):
         self.app = app
         self.baseDir = r"\\allen\programs\mindscope\workgroups\dynamicrouting\DynamicRoutingTask"
+        self.bregmaGalvoData = self.getBregmaGalvoData()
         
         winHeight = 150
         winWidth = 150
@@ -39,28 +42,39 @@ class OptoGui():
         self.rigNameLabel.setAlignment(QtCore.Qt.AlignVCenter)
         self.rigNameEdit = QtWidgets.QLineEdit('NP3')
         self.rigNameEdit.setAlignment(QtCore.Qt.AlignHCenter)
-
-        self.galvoXLabel = QtWidgets.QLabel('Galvo X (-5-5 V):')
-        self.galvoXLabel.setAlignment(QtCore.Qt.AlignVCenter)
-        self.galvoXEdit = QtWidgets.QLineEdit('-0.2')
-        self.galvoXEdit.setAlignment(QtCore.Qt.AlignHCenter)
-        self.galvoXEdit.editingFinished.connect(self.setGalvoValue)
         
-        self.galvoYLabel = QtWidgets.QLabel('Galvo Y (-5-5 V):')
-        self.galvoYLabel.setAlignment(QtCore.Qt.AlignVCenter)
-        self.galvoYEdit = QtWidgets.QLineEdit('-2')
-        self.galvoYEdit.setAlignment(QtCore.Qt.AlignHCenter)
-        self.galvoYEdit.editingFinished.connect(self.setGalvoValue)
+        self.galvoButton = QtWidgets.QRadioButton('Galvo (V)')
+        self.bregmaButton = QtWidgets.QRadioButton('Bregma (mm)')
+        self.galvoButton.setChecked(True)
+        self.galvoMode = True
+        self.modeLayout = QtWidgets.QHBoxLayout()
+        for button in (self.galvoButton,self.bregmaButton):
+            button.clicked.connect(self.setMode)
+            self.modeLayout.addWidget(button)
+        self.modeGroupBox = QtWidgets.QGroupBox()
+        self.modeGroupBox.setLayout(self.modeLayout)
+
+        self.xLabel = QtWidgets.QLabel('X:')
+        self.xLabel.setAlignment(QtCore.Qt.AlignVCenter)
+        self.xEdit = QtWidgets.QLineEdit('-0.2')
+        self.xEdit.setAlignment(QtCore.Qt.AlignHCenter)
+        self.xEdit.editingFinished.connect(self.setXYValue)
+        
+        self.yLabel = QtWidgets.QLabel('Y:')
+        self.yLabel.setAlignment(QtCore.Qt.AlignVCenter)
+        self.yEdit = QtWidgets.QLineEdit('-2')
+        self.yEdit.setAlignment(QtCore.Qt.AlignHCenter)
+        self.yEdit.editingFinished.connect(self.setXYValue)
         
         self.laserAmpLabel = QtWidgets.QLabel('Laser Amplitude (0-5 V):')
         self.laserAmpLabel.setAlignment(QtCore.Qt.AlignVCenter)
-        self.laserAmpEdit = QtWidgets.QLineEdit('0')
+        self.laserAmpEdit = QtWidgets.QLineEdit('0.2')
         self.laserAmpEdit.setAlignment(QtCore.Qt.AlignHCenter)
         self.laserAmpEdit.editingFinished.connect(self.setLaserAmpValue)
 
         self.laserDurLabel = QtWidgets.QLabel('Laser Duration (s):')
         self.laserDurLabel.setAlignment(QtCore.Qt.AlignVCenter)
-        self.laserDurEdit = QtWidgets.QLineEdit('2')
+        self.laserDurEdit = QtWidgets.QLineEdit('1')
         self.laserDurEdit.setAlignment(QtCore.Qt.AlignHCenter)
         self.laserDurEdit.editingFinished.connect(self.setLaserDurValue)
         
@@ -70,18 +84,19 @@ class OptoGui():
         self.mainWidget = QtWidgets.QWidget()
         self.mainWin.setCentralWidget(self.mainWidget)
         self.mainLayout = QtWidgets.QGridLayout()
-        self.setLayoutGridSpacing(self.mainLayout,winHeight,winWidth,6,2)
+        self.setLayoutGridSpacing(self.mainLayout,winHeight,winWidth,7,2)
         self.mainLayout.addWidget(self.rigNameLabel,0,0,1,1)
         self.mainLayout.addWidget(self.rigNameEdit,0,1,1,1)
-        self.mainLayout.addWidget(self.galvoXLabel,1,0,1,1)
-        self.mainLayout.addWidget(self.galvoXEdit,1,1,1,1)
-        self.mainLayout.addWidget(self.galvoYLabel,2,0,1,1)
-        self.mainLayout.addWidget(self.galvoYEdit,2,1,1,1)
-        self.mainLayout.addWidget(self.laserAmpLabel,3,0,1,1)
-        self.mainLayout.addWidget(self.laserAmpEdit,3,1,1,1)
-        self.mainLayout.addWidget(self.laserDurLabel,4,0,1,1)
-        self.mainLayout.addWidget(self.laserDurEdit,4,1,1,1)
-        self.mainLayout.addWidget(self.applyValuesButton,5,0,1,2)
+        self.mainLayout.addWidget(self.modeGroupBox,1,0,1,2)
+        self.mainLayout.addWidget(self.xLabel,2,0,1,1)
+        self.mainLayout.addWidget(self.xEdit,2,1,1,1)
+        self.mainLayout.addWidget(self.yLabel,3,0,1,1)
+        self.mainLayout.addWidget(self.yEdit,3,1,1,1)
+        self.mainLayout.addWidget(self.laserAmpLabel,4,0,1,1)
+        self.mainLayout.addWidget(self.laserAmpEdit,4,1,1,1)
+        self.mainLayout.addWidget(self.laserDurLabel,5,0,1,1)
+        self.mainLayout.addWidget(self.laserDurEdit,5,1,1,1)
+        self.mainLayout.addWidget(self.applyValuesButton,6,0,1,2)
         self.mainWidget.setLayout(self.mainLayout)
         
         self.mainWin.show()
@@ -93,14 +108,25 @@ class OptoGui():
         for col in range(cols):
             layout.setColumnMinimumWidth(col,int(width/cols))
             layout.setColumnStretch(col,1)
+            
+    def setMode(self):
+        sender = self.mainWin.sender()
+        if (sender==self.galvoButton and not self.galvoMode) or (sender==self.bregmaButton and self.galvoMode):
+            self.galvoMode = not self.galvoMode
+            x = float(self.xEdit.text())
+            y = float(self.yEdit.text())
+            x,y = self.bregmaToGalvo(x,y) if self.galvoMode else self.galvoToBregma(x,y)
+            self.xEdit.setText(str(round(x,3)))
+            self.yEdit.setText(str(round(y,3)))
 
-    def setGalvoValue(self):
+    def setXYValue(self):
         sender = self.mainWin.sender()
         val = float(sender.text())
-        if val < -5:
-            sender.setText('-5')
-        elif val > 5:
-            sender.setText('5')
+        if self.galvoMode:
+            if val < -5:
+                sender.setText('-5')
+            elif val > 5:
+                sender.setText('5')
         
     def setLaserAmpValue(self):
         val = float(self.laserAmpEdit.text())
@@ -119,16 +145,18 @@ class OptoGui():
         scriptPath = os.path.join(self.baseDir,'OptoGui','startOptoTask.py')
         taskScript = os.path.join(self.baseDir,'TaskControl.py')
         taskVersion = 'opto test'
-        galvoX = self.galvoXEdit.text()
-        galvoY = self.galvoYEdit.text()
+        x = self.xEdit.text()
+        y = self.yEdit.text()
+        if not self.galvoMode:
+            x,y = [str(n) for n in self.bregmaToGalvo(float(x),float(y))]
         optoAmp = self.laserAmpEdit.text()
         optoDur = self.laserDurEdit.text()
         batString = ('python ' + '"' + scriptPath +'"' + 
                      ' --rigName ' + '"' + rigName + '"' + 
                      ' --taskScript ' + '"' + taskScript + '"' + 
                      ' --taskVersion ' + '"' + taskVersion + '"' +
-                     ' --galvoX ' + galvoX +
-                     ' --galvoY ' + galvoY +
+                     ' --galvoX ' + x +
+                     ' --galvoY ' + y +
                      ' --optoAmp ' + optoAmp +
                      ' --optoDur ' + optoDur)
         self.runBatFile(batString)
@@ -144,6 +172,31 @@ class OptoGui():
             
         p = subprocess.Popen([batFile])
         p.wait()
+
+    def getBregmaGalvoData(self):
+        f = r"\\allen\programs\mindscope\workgroups\dynamicrouting\DynamicRoutingTask\OptoGui\NP3_bregma_galvo.txt"
+        d = pd.read_csv(f,sep='\t')
+        
+        bregmaToGalvoFit = np.linalg.lstsq(np.concatenate((d[['bregma x','bregma y']],np.ones(d.shape[0])[:,None]),axis=1),
+                                           d[['galvo x','galvo y']])[0].T
+
+        galvoToBregmaFit = np.linalg.lstsq(np.concatenate((d[['galvo x','galvo y']],np.ones(d.shape[0])[:,None]),axis=1),
+                                           d[['bregma x','bregma y']])[0].T
+        
+        return bregmaToGalvoFit,galvoToBregmaFit
+
+    def bregmaToGalvo(self,bregmaX,bregmaY):
+        bregmaToGalvoFit = self.bregmaGalvoData[0]
+        galvoX = bregmaToGalvoFit[0,0]*bregmaX + bregmaToGalvoFit[0,1]*bregmaY + bregmaToGalvoFit[0,2]
+        galvoY = bregmaToGalvoFit[1,0]*bregmaX + bregmaToGalvoFit[1,1]*bregmaY + bregmaToGalvoFit[1,2]
+        return galvoX,galvoY
+    
+    def galvoToBregma(self,galvoX,galvoY):
+        galvoToBregmaFit = self.bregmaGalvoData[1]
+        bregmaX = galvoToBregmaFit[0,0]*galvoX + galvoToBregmaFit[0,1]*galvoY + galvoToBregmaFit[0,2]
+        bregmaY = galvoToBregmaFit[1,0]*galvoX + galvoToBregmaFit[1,1]*galvoY + galvoToBregmaFit[1,2]
+        return bregmaX,bregmaY
+
 
 if __name__=="__main__":
     start()
--- OptoGui/OptoGui_env.bat
+++ OptoGui/OptoGui_env.bat
@@ -1,3 +1,6 @@
 call conda create --name OptoGui python=3 --yes
 call activate OptoGui
 call python -m pip install PyQt5==5.15.6 --index-url https://pypi.org/simple
+call python -m pip install numpy==1.22.3 --index-url https://pypi.org/simple
+call python -m pip install pandas==1.4.2 --index-url https://pypi.org/simple
+cmd /k
