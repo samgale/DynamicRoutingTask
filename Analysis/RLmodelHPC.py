2f78d5955154346643c436245200c109a3835cce
Sam Gale <samueldgale@gmail.com>
2023-12-20 17:44:28 -0800

    Update RLmodelHPC.py

--- Analysis/RLmodelHPC.py
+++ Analysis/RLmodelHPC.py
@@ -119,11 +119,10 @@ def runModel(obj,tauAction,biasAction,visConfidence,audConfidence,alphaContext,a
 def evalModel(params,*args):
     trainExps,fixedValInd,fixedVal = args
     if fixedVal is not None:
-        params = np.insert(params,fixedValInd,fixedVal)
-    actualResponse = np.concatenate([obj.trialResponse for obj in trainExps])
-    pAction = np.concatenate([runModel(obj,*params)[4][0] for obj in trainExps])
-    print(params)
-    logLoss = sklearn.metrics.log_loss(actualResponse,pAction)
+        params = np.insert(params,(fixedValInd[0] if isinstance(fixedValInd,tuple) else fixedValInd),fixedVal)
+    response = np.concatenate([obj.trialResponse for obj in trainExps])
+    prediction = np.concatenate([runModel(obj,*params)[4][0] for obj in trainExps])
+    logLoss = sklearn.metrics.log_loss(response,prediction)
     return logLoss
 
 
@@ -141,16 +140,17 @@ def fitModel(mouseId,sessionData,sessionIndex,trainingPhase):
 
     bounds = (tauActionBounds,biasActionBounds,visConfidenceBounds,audConfidenceBounds,alphaContextBounds,alphaActionBounds,alphaHabitBounds)
 
-    fixedValues = (None,None,1,1,0,0,0)
+    fixedValueIndices = (None,None,2,3,4,5,(4,5),6)
+    fixedValues = (None,None,1,1,0,0,(0,0),0)
 
     fit = scipy.optimize.direct(evalModel,bounds,args=(trainExps,None,None))
     params = [fit.x]
     logLoss = [fit.fun]
-    for fixedValInd,fixedVal in enumerate(fixedValues):
+    for fixedValInd,fixedVal in zip(fixedValueIndices,fixedValues):
         if fixedVal is not None:
-            bnds = tuple(b for i,b in enumerate(bounds) if i != fixedValInd)
+            bnds = tuple(b for i,b in enumerate(bounds) if (i not in fixedValInd if isinstance(fixedValInd,tuple) else i != fixedValInd))
             fit = scipy.optimize.direct(evalModel,bnds,args=(trainExps,fixedValInd,fixedVal))
-            params.append(np.insert(fit.x,fixedValInd,fixedVal))
+            params.append(np.insert(fit.x,(fixedValInd[0] if isinstance(fixedValInd,tuple) else fixedValInd),fixedVal))
             logLoss.append(fit.fun)
 
     fileName = str(mouseId)+'_'+testExp.startTime+'_'+trainingPhase+'.npz'
