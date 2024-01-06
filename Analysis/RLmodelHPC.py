5ddf6e67f98e0b28ef47df844527517206e12d4f
Sam Gale <samueldgale@gmail.com>
2024-01-05 18:27:04 -0800

    Update RLmodelHPC.py

--- Analysis/RLmodelHPC.py
+++ Analysis/RLmodelHPC.py
@@ -50,7 +50,9 @@ def calcLogisticProb(q,tau,bias):
     return 1 / (1 + np.exp(-(q + bias) / tau))
 
 
-def runModel(obj,tauAction,biasAction,visConfidence,audConfidence,alphaContext,alphaAction,alphaHabit,weightContext=False,useHistory=True,nReps=1):
+def runModel(obj,tauAction,biasAction,visConfidence,audConfidence,alphaContext,alphaAction,alphaHabit,
+             weightContext=False,weightAction=False,weightLearning=False,attendReward=False,useRPE=False,
+             useHistory=True,nReps=1):
     stimNames = ('vis1','vis2','sound1','sound2')
     stimConfidence = [visConfidence,audConfidence]
 
@@ -82,16 +84,21 @@ def runModel(obj,tauAction,biasAction,visConfidence,audConfidence,alphaContext,a
                 pStim = np.zeros(len(stimNames))
                 pStim[[stim[:-1] in s for s in stimNames]] = [stimConfidence[modality],1-stimConfidence[modality]] if '1' in stim else [1-stimConfidence[modality],stimConfidence[modality]]
 
-                valContext = np.sum(qContext[i,trial] * pStim[None,:] * pContext[i,trial][:,None])
-                valStim = np.sum(qStim[i,trial] * pStim)
-                if weightContext:
-                    wContext = 1 - 2 * pContext[i,trial].min()
-                    expectedValue[i,trial] = wContext * valContext  + (1 - wContext) * valStim 
-                else:   
-                    if alphaContext > 0:
-                        expectedValue[i,trial] = valContext
-                    else:
+                if weightAction:
+                    expectedValue[i,trial] = np.sum(qStim[i,trial] * pStim * np.repeat(pContext[i,trial],2))
+                else:
+                    valContext = np.sum(qContext[i,trial] * pStim[None,:] * pContext[i,trial][:,None])
+                    valStim = np.sum(qStim[i,trial] * pStim)
+                    if attendReward:
                         expectedValue[i,trial] = valStim
+                    if weightContext:
+                        wContext = 1 - 2 * pContext[i,trial].min()
+                        expectedValue[i,trial] = wContext * valContext  + (1 - wContext) * valStim 
+                    else:   
+                        if alphaContext > 0:
+                            expectedValue[i,trial] = valContext
+                        else:
+                            expectedValue[i,trial] = valStim
 
                 q = (wHabit[i,trial] * np.sum(qHabit * pStim)) + ((1 - wHabit[i,trial]) * expectedValue[i,trial])           
             
@@ -113,7 +120,9 @@ def runModel(obj,tauAction,biasAction,visConfidence,audConfidence,alphaContext,a
                     predictionError = outcome - expectedValue[i,trial]
                     
                     if alphaContext > 0 and stim != 'catch':
-                        if True:#useRPE:
+                        if attendReward:
+                            contextError = outcome
+                        elif useRPE:
                             contextError = 0.5 * predictionError
                         else:
                             if outcome < 1:
@@ -125,8 +134,11 @@ def runModel(obj,tauAction,biasAction,visConfidence,audConfidence,alphaContext,a
                         pContext[i,trial+1,(1 if modality==0 else 0)] = 1 - pContext[i,trial+1,modality]
                     
                     if alphaAction > 0 and stim != 'catch':
-                        if weightContext or alphaContext == 0:
-                            qStim[i,trial+1] += alphaAction * pStim * predictionError
+                        if attendReward or weightAction or weightContext or alphaContext == 0:
+                            dq = alphaAction * pStim * predictionError
+                            if weightLearning:
+                                dq *= pContext[i,trial,modality]
+                            qStim[i,trial+1] += dq
                             qStim[i,trial+1] = np.clip(qStim[i,trial+1],-1,1)
                         else:
                             qContext[i,trial+1] += alphaAction * pContext[i,trial][:,None] * pStim[None,:] * predictionError
@@ -139,11 +151,11 @@ def runModel(obj,tauAction,biasAction,visConfidence,audConfidence,alphaContext,a
 
 
 def evalModel(params,*args):
-    trainData,fixedValInd,fixedVal,weightContext = args
+    trainData,fixedValInd,fixedVal,modelTypeDict = args
     if fixedVal is not None:
         params = np.insert(params,(fixedValInd[0] if isinstance(fixedValInd,tuple) else fixedValInd),fixedVal)
     response = np.concatenate([obj.trialResponse for obj in trainData])
-    prediction = np.concatenate([runModel(obj,*params,weightContext=weightContext)[-2][0] for obj in trainData])
+    prediction = np.concatenate([runModel(obj,*params,**modelTypeDict)[-2][0] for obj in trainData])
     logLoss = sklearn.metrics.log_loss(response,prediction)
     return logLoss
 
@@ -163,20 +175,31 @@ def fitModel(mouseId,trainingPhase,testData,trainData):
     fixedValueIndices = (None,1,2,3,4,5,(4,5),6)
     fixedValues = (None,0,1,1,0,0,(0,0),0)
 
-    for weightContext in (False,True):
-        fit = scipy.optimize.direct(evalModel,bounds,args=(trainData,None,None,weightContext))
+    modelTypeParamNames = ('weightContext','weightAction','weightLearning','attendReward','useRPE')
+    modelTypeNames,modelTypes = zip(
+                                    ('contextQ',(0,0,0,0,1)),
+                                    ('weightContext',(1,0,0,0,1)),
+                                    ('weightAction',(0,1,0,0,1)),
+                                    ('attendActionLearn',(0,1,1,1,0)),
+                                    ('attendAction',(0,1,0,1,0)),
+                                    ('attendLearn',(0,0,1,1,0)),
+                                   )
+
+    for modelTypeName,modelType in zip(modelTypeNames,modelTypes):
+        modelTypeParams = {p: bool(m) for p,m in zip(modelTypeParamNames,modelType)}
+        fit = scipy.optimize.direct(evalModel,bounds,args=(trainData,None,None,modelTypeParams))
         params = [fit.x]
         logLoss = [fit.fun]
         for fixedValInd,fixedVal in zip(fixedValueIndices,fixedValues):
             if fixedVal is not None:
                 bnds = tuple(b for i,b in enumerate(bounds) if (i not in fixedValInd if isinstance(fixedValInd,tuple) else i != fixedValInd))
-                fit = scipy.optimize.direct(evalModel,bnds,args=(trainData,fixedValInd,fixedVal,weightContext))
+                fit = scipy.optimize.direct(evalModel,bnds,args=(trainData,fixedValInd,fixedVal,modelTypeParams))
                 params.append(np.insert(fit.x,(fixedValInd[0] if isinstance(fixedValInd,tuple) else fixedValInd),fixedVal))
                 logLoss.append(fit.fun)
 
-        fileName = str(mouseId)+'_'+testData.startTime+'_'+trainingPhase+'_weightContext'+str(weightContext)+'.npz'
+        fileName = str(mouseId)+'_'+testData.startTime+'_'+trainingPhase+'_'+modelTypeName+'.npz'
         filePath = os.path.join(baseDir,'Sam','RLmodel',fileName)
-        np.savez(filePath,params=params,logLoss=logLoss) 
+        np.savez(filePath,params=params,logLoss=logLoss,**modelTypeParams) 
         
 
 if __name__ == "__main__":
