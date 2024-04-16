49d621a2f0ff3ba7129bf40dce263d6ea453c506
Sam Gale <samueldgale@gmail.com>
2024-04-16 09:56:20 -0700

    Update npc_analysis_2.py

--- Analysis/npc_analysis_2.py
+++ Analysis/npc_analysis_2.py
@@ -19,47 +19,61 @@ stimLatency = obj.stim_start_time - trialStartTime
 
 optoLatency = obj.opto_start_time - trialStartTime
 
-optoStimOffset = stimLatency - optoLatency
+optoStimOffset = optoLatency - stimLatency
 
 #%%
 fig = plt.figure()
 gs = matplotlib.gridspec.GridSpec(3,2)
-bins = np.arange(-60,60)/1000
-for i,(t,lbl) in enumerate(zip((stimLatency,optoLatency,optoStimOffset),
+bins = np.arange(-40,40)/1000
+for i,(t,xlbl) in enumerate(zip((stimLatency,optoLatency,optoStimOffset),
                                ('stimulus latency','opto latency','opto offset'))):
     for j,(stimTrials,stim) in enumerate(zip((obj.is_vis_stim,obj.is_aud_stim),
                                              ('visual','auditory'))):
         ax = fig.add_subplot(gs[i,j])
-        trials = stimTrials & obj.is_opto if i>0 else stimTrials
-        ax.hist(t[trials],bins=bins,color='k')
-        ax.set_xlabel(lbl)
-        if i==0:
-            ax.set_title(stim)
-        if i==1 and j==0:
-            ax.set_ylabel('# of trials')
+        for trials,clr,lbl in zip((stimTrials & ~obj.is_opto,stimTrials & obj.is_opto),'kb',('no opto','opto')):
+            if i > 0 and lbl == 'no opto':
+                continue
+            h,b = np.histogram(t[trials],bins=bins)
+            ax.plot(bins[:-1],h,color=clr,label=lbl)
         for side in ('right','top'):
             ax.spines[side].set_visible(False)
         ax.tick_params(direction='out',top=False,right=False)
+        ax.set_xlabel(xlbl)
+        if i==1 and j==0:
+            ax.set_ylabel('# trials')
+        if i==0:
+            ax.set_title(stim)
+        if i==0 and j==1:
+            ax.legend(loc='upper right')
 plt.tight_layout()
 
 #%%
 fig = plt.figure()
 gs = matplotlib.gridspec.GridSpec(2,2)
-bins = np.arange(100)/1000
+bins = np.arange(55)/1000
 trialStartIndex = np.array([np.where(obj._flip_times==t)[0][0] for t in trialStartTime])
-pre = trialStartTime - obj._flip_times[trialStartIndex-2]
-post = obj._flip_times[trialStartIndex+2] - trialStartTime
+pre = trialStartTime - obj._flip_times[trialStartIndex-1]
+post = obj._flip_times[trialStartIndex+1] - trialStartTime
 for i,(stimTrials,stim) in enumerate(zip((obj.is_vis_stim,obj.is_aud_stim),
                                          ('visual','auditory'))):
-    for j,(t,lbl) in enumerate(zip((pre,post),('pre','post'))):
+    for j,t in enumerate((pre,post)):
         ax = fig.add_subplot(gs[i,j])
-        trials = stimTrials & obj.is_opto if i>0 else stimTrials
-        for trials,clr in zip((stimTrials & ~obj.is_opto,stimTrials & obj.is_opto),'kb'):
+        for trials,clr,lbl in zip((stimTrials & ~obj.is_opto,stimTrials & obj.is_opto),'kb',('no opto','opto')):
             h,b = np.histogram(t[trials],bins=bins)
-            ax.plot(bins[:-1],h,color=clr)
+            ax.plot(bins[:-1],h,color=clr,label=lbl)
         for side in ('right','top'):
             ax.spines[side].set_visible(False)
         ax.tick_params(direction='out',top=False,right=False)
+        if i==1:
+            if j==0:
+                ax.set_xlabel('Frame interval before trial start (s)')
+            else:
+                ax.set_xlabel('Frame interval after trial start (s)')
+        if j==0:
+            ax.set_ylabel('# trials')
+        if i==0 and j==1:
+            ax.legend(loc='upper right')
+        ax.set_title(stim)
 plt.tight_layout()
 
 
