7aef73ded6df052cc1afcead917d4e31382e6431
Sam Gale <samueldgale@gmail.com>
2026-01-28 13:28:33 -0800

    sdg

--- Analysis/RNNmodel.py
+++ Analysis/RNNmodel.py
@@ -5,6 +5,7 @@ Created on Tue Dec  2 17:38:41 2025
 @author: svc_ccg
 """
 
+import copy
 import glob
 import os
 import random
@@ -306,6 +307,137 @@ for hiddenType in hiddenTypes:
 
 
 
+# intra-block resp correlations
+def getBlockTrials(obj,block,epoch):
+    blockTrials = (obj.trialBlock==block) & ~obj.autoRewardScheduled
+    n = blockTrials.sum()
+    half = int(n/2)
+    startTrial = half if epoch=='last half' else 0
+    endTrial = half if epoch=='first half' else n
+    return np.where(blockTrials)[0][startTrial:endTrial]
 
+
+def detrend(r,order=2):
+    x = np.arange(r.size)
+    return r - np.polyval(np.polyfit(x,r,order),x)
+
+
+def getCorrelation(r1,r2,rs1,rs2,corrSize=200,detrendOrder=None):
+    if detrendOrder is not None:
+        r1 = detrend(r1,detrendOrder)
+        r2 = detrend(r2,detrendOrder)
+        rs1 = rs1.copy()
+        rs2 = rs2.copy()
+        for z in range(rs1.shape[1]):
+            rs1[:,z] = detrend(rs1[:,z],detrendOrder)
+            rs2[:,z] = detrend(rs2[:,z],detrendOrder)
+    c = np.correlate(r1,r2,'full') / (np.linalg.norm(r1) * np.linalg.norm(r2))   
+    cs = np.mean([np.correlate(rs1[:,z],rs2[:,z],'full') / (np.linalg.norm(rs1[:,z]) * np.linalg.norm(rs2[:,z])) for z in range(rs1.shape[1])],axis=0)
+    n = c.size // 2 + 1
+    corrRaw = np.full(corrSize,np.nan)
+    corrRaw[:n] = c[-n:]
+    corr = np.full(corrSize,np.nan)
+    corr[:n] = (c-cs)[-n:] 
+    return corr,corrRaw
+
+epoch = 'full'
+stimNames = ('vis1','sound1','vis2','sound2')
+autoCorrMat = {src: np.zeros((4,len(modelData),100)) for src in ('mice','model')}
+autoCorrDetrendMat = copy.deepcopy(autoCorrMat)
+corrWithinMat = {src: np.zeros((4,4,len(modelData),200)) for src in ('mice','model')}
+corrWithinDetrendMat = copy.deepcopy(corrWithinMat)
+minTrials = 3
+nShuffles = 10
+for src in ('mice','model'):
+    for m,mouseId in enumerate(modelData):
+        if any([modelData[mouseId][session]['isComplete'] for session in modelData[mouseId]]):
+            autoCorr = [[] for _ in range(4)]
+            autoCorrDetrend = copy.deepcopy(autoCorr)
+            corrWithin = [[[] for _ in range(4)] for _ in range(4)]
+            corrWithinDetrend = copy.deepcopy(corrWithin)
+            for session in modelData[mouseId]:
+                if modelData[mouseId][session]['isComplete']:
+                    obj = sessionData[mouseId][session]
+                    if src=='mice': 
+                        trialResponse = [obj.trialResponse]
+                    else:    
+                        trialResponse = modelData[mouseId][session][hiddenType][16][16]['simAction']
+                    for tr in trialResponse:
+                        resp = np.zeros((4,obj.nTrials))
+                        respShuffled = np.zeros((4,obj.nTrials,nShuffles))
+                        for blockInd,rewStim in enumerate(obj.blockStimRewarded):
+                            blockTrials = getBlockTrials(obj,blockInd+1,epoch)
+                            for i,s in enumerate(stimNames if rewStim=='vis1' else ('sound1','vis1','sound2','vis2')):
+                                stimTrials = np.intersect1d(blockTrials,np.where(obj.trialStim==s)[0])
+                                if len(stimTrials) < minTrials:
+                                    continue
+                                r = tr[stimTrials].astype(float)
+                                r[r<1] = -1
+                                resp[i,stimTrials] = r
+                                for z in range(nShuffles):
+                                    respShuffled[i,stimTrials,z] = np.random.permutation(r)
+                        
+                        for blockInd,rewStim in enumerate(obj.blockStimRewarded):
+                            blockTrials = getBlockTrials(obj,blockInd+1,epoch)
+                            for i,s in enumerate(stimNames if rewStim=='vis1' else ('sound1','vis1','sound2','vis2')):
+                                stimTrials = np.intersect1d(blockTrials,np.where(obj.trialStim==s)[0])
+                                if len(stimTrials) < minTrials:
+                                    continue
+                                r = resp[i,stimTrials]
+                                rs = respShuffled[i,stimTrials]
+                                corr,corrRaw = getCorrelation(r,r,rs,rs,100)
+                                autoCorr[i].append(corr)
+                                corrDetrend,corrRawDetrend = getCorrelation(r,r,rs,rs,100,detrendOrder=2)
+                                autoCorrDetrend[i].append(corrDetrend)
+                            
+                            r = resp[:,blockTrials]
+                            rs = respShuffled[:,blockTrials]
+                            for i,(r1,rs1) in enumerate(zip(r,rs)):
+                                for j,(r2,rs2) in enumerate(zip(r,rs)):
+                                    if np.count_nonzero(r1) >= minTrials and np.count_nonzero(r2) >= minTrials:
+                                        corr,corrRaw = getCorrelation(r1,r2,rs1,rs2)
+                                        corrWithin[i][j].append(corr)
+                                        corrDetrend,corrRawDetrend = getCorrelation(r1,r2,rs1,rs2,detrendOrder=2)
+                                        corrWithinDetrend[i][j].append(corrDetrend)
+                       
+            autoCorrMat[src][:,m] = np.nanmean(autoCorr,axis=1)
+            autoCorrDetrendMat[src][:,m] = np.nanmean(autoCorrDetrend,axis=1)
+                    
+            corrWithinMat[src][:,:,m] = np.nanmean(corrWithin,axis=2)
+            corrWithinDetrendMat[src][:,:,m] = np.nanmean(corrWithinDetrend,axis=2)
+
+stimLabels = ('rewarded target','unrewarded target','non-target\n(rewarded modality)','non-target\n(unrewarded modality)')
+
+
+fig = plt.figure(figsize=(12,10))       
+gs = matplotlib.gridspec.GridSpec(4,4)
+x = np.arange(1,200)
+for i,ylbl in enumerate(stimLabels):
+    for j,xlbl in enumerate(stimLabels[:4]):
+        ax = fig.add_subplot(gs[i,j])
+        for lbl,clr in zip(('mice','model'),'kr'):
+            mat = corrWithinDetrendMat[lbl][i,j,:,1:]
+            m = np.nanmean(mat,axis=0)
+            s = np.nanstd(mat,axis=0) / (len(mat) ** 0.5)
+            ax.plot(x,m,clr,label=lbl)
+            ax.fill_between(x,m-s,m+s,color=clr,alpha=0.25)
+        for side in ('right','top'):
+            ax.spines[side].set_visible(False)
+        ax.tick_params(direction='out',top=False,right=False,labelsize=12)
+        ax.set_xlim([0,20])
+        ax.set_ylim([-0.03,0.05])
+        if i==3:
+            ax.set_xlabel('Lag (trials)',fontsize=14)
+        else:
+            ax.set_xticklabels([])
+        if j==0:
+            ax.set_ylabel(ylbl,fontsize=14)
+        else:
+            ax.set_yticklabels([])
+        if i==0:
+            ax.set_title(xlbl,fontsize=14)
+        if i==0 and j==3:
+            ax.legend(bbox_to_anchor=(1,1),loc='upper left',fontsize=14)
+plt.tight_layout()
 
 
--- Analysis/disrnn.py
+++ Analysis/disrnn.py
@@ -42,111 +42,115 @@ for mouseId in mice:
         mouseIds.append(mouseId)
         sessionStartTimes.append([st.strftime('%Y%m%d_%H%M%S') for st in df.loc[sessions,'start time']])
 
-sessionData = [getSessionData(mouseIds[0],st) for st in sessionStartTimes[0]][:22]
-maxTrials = max(session.nTrials for session in sessionData)
-nInputs = 6
-stimNames = ['vis1','vis2','sound1','sound2']
 
-modelInput = -1 * np.ones((maxTrials,len(sessionData),nInputs),dtype=np.float32)
-targetOutput = -1 * np.ones((maxTrials,len(sessionData),1),dtype=np.int32)
-for i,session in enumerate(sessionData):
-    n = session.nTrials
-    for j,stim in enumerate(stimNames):    
-        modelInput[:n,i,j] = session.trialStim == stim
-    modelInput[0,i,4:6] = 0
-    modelInput[1:n,i,4] = session.trialResponse[:-1]
-    modelInput[1:n,i,5] = session.trialRewarded[:-1]
-    targetOutput[:n,i,0] = session.trialResponse
+def getDisrnnDataset(sessionData,testIndex):
+    nInputs = 6
+    stimNames = ['vis1','vis2','sound1','sound2']
+    maxTrials = max(session.nTrials for session in sessionData)
+    modelInput = -1 * np.ones((maxTrials,len(sessionData),nInputs),dtype=np.float32)
+    targetOutput = -1 * np.ones((maxTrials,len(sessionData),1),dtype=np.int32)
+    for i,session in enumerate(sessionData):
+        n = session.nTrials
+        for j,stim in enumerate(stimNames):    
+            modelInput[:n,i,j] = session.trialStim == stim
+        modelInput[0,i,4:6] = 0
+        modelInput[1:n,i,4] = session.trialResponse[:-1]
+        modelInput[1:n,i,5] = session.trialRewarded[:-1]
+        targetOutput[:n,i,0] = session.trialResponse
     
-testInd = [2,3]
-trainInd = np.setdiff1d(np.arange(len(sessionData)),testInd)    
-testDataset,trainDataset = [rnn_utils.DatasetRNN(
-        xs=modelInput[:,i],
-        ys=targetOutput[:,i],
-        y_type='categorical',
-        n_classes=2,
-        x_names=stimNames+['prev resp','prev outcome'],
-        y_names=['resp'],
-        batch_size=1,
-        batch_mode='random')
-    for i in (testInd,trainInd)]
+    trainIndex = np.setdiff1d(np.arange(len(sessionData)),testIndex)    
+    testDataset,trainDataset = [rnn_utils.DatasetRNN(
+            xs=modelInput[:,i],
+            ys=targetOutput[:,i],
+            y_type='categorical',
+            n_classes=2,
+            x_names=stimNames+['prev resp','prev outcome'],
+            y_names=['resp'],
+            batch_size=1,
+            batch_mode='random')
+        for i in (testIndex,trainIndex)]
+    return testDataset,trainDataset
+    
+
+modelParams = []
+configs = []
+for mouseId,sessions in zip(mouseIds[:1],sessionStartTimes[:1]):
+    sessionData = [getSessionData(mouseId,st) for st in sessions[:21]]
+    testIndex = [2]
+    testDataset,trainDataset = getDisrnnDataset(sessionData,testIndex)
+
+    # define the disRNN architecture
+    disrnn_config = disrnn.DisRnnConfig(
+        # Dataset related
+        obs_size=6,
+        output_size=2,
+        x_names=testDataset.x_names,
+        y_names=testDataset.y_names,
+        # Network architecture
+        latent_size=6,
+        update_net_n_units_per_layer=16,
+        update_net_n_layers=4,
+        choice_net_n_units_per_layer=4,
+        choice_net_n_layers=2,
+        activation="leaky_relu",
+        # Penalties
+        noiseless_mode=False,
+        latent_penalty=0.001, # 0.001
+        update_net_obs_penalty=0.001, # 0.01
+        update_net_latent_penalty=0.01, # 0.01
+        choice_net_latent_penalty=0.001, # 0.001
+        l2_scale=1e-5)
+
+    # Define a config for warmup training with no noise and no penalties
+    disrnn_config_warmup = copy.deepcopy(disrnn_config)
+    disrnn_config_warmup.latent_penalty = 0
+    disrnn_config_warmup.choice_net_latent_penalty = 0
+    disrnn_config_warmup.update_net_obs_penalty = 0
+    disrnn_config_warmup.update_net_latent_penalty = 0
+    disrnn_config_warmup.l2_scale = 0
+    disrnn_config_warmup.noiseless_mode = True
+
+    # Define network builder functions
+    make_disrnn = lambda: disrnn.HkDisentangledRNN(disrnn_config)
+    make_disrnn_warmup = lambda: disrnn.HkDisentangledRNN(disrnn_config_warmup)
+
+    # Define an optimizer
+    opt = optax.adam(learning_rate=0.001)
+
+    # Warmup training with no noise and no penalties
+    params, _, _ = rnn_utils.train_network(
+        make_disrnn_warmup,
+        training_dataset=trainDataset,
+        validation_dataset=testDataset,
+        loss="penalized_categorical",
+        params=None,
+        opt_state=None,
+        opt=opt,
+        n_steps=1000,
+        do_plot=False)
+
+    # Additional training using information penalty
+    params, _, _ = rnn_utils.train_network(
+        make_disrnn,
+        training_dataset=trainDataset,
+        validation_dataset=testDataset,
+        loss="penalized_categorical",
+        params=params,
+        opt_state=None,
+        opt=opt,
+        n_steps=10000,
+        do_plot=True)
+    
+    modelParams.append(params)
+    configs.append(disrnn_config)
 
 
-#
-# FLAGS = flags.FLAGS
-# flags.DEFINE_integer("n_steps_per_session", maxTrials, "Number of steps per session in the dataset.")
-# flags.DEFINE_integer("n_sessions", len(sessionData), "Number of sessions in the dataset.")
-# flags.DEFINE_float("learning_rate", 1e-3, "Optimizer learning rate.")
-# flags.DEFINE_integer("n_warmup_steps", 1000, "Number of training warmup steps.")
-# flags.DEFINE_integer("n_training_steps", 3000, "Number of main training steps.")
-
-
-# define the disRNN architecture
-disrnn_config = disrnn.DisRnnConfig(
-    # Dataset related
-    obs_size=nInputs,
-    output_size=2,
-    x_names=testDataset.x_names,
-    y_names=testDataset.y_names,
-    # Network architecture
-    latent_size=6,
-    update_net_n_units_per_layer=16,
-    update_net_n_layers=4,
-    choice_net_n_units_per_layer=2,
-    choice_net_n_layers=1,
-    activation="leaky_relu",
-    # Penalties
-    noiseless_mode=False,
-    latent_penalty=0.001, # 0.001
-    update_net_obs_penalty=0.01, # 0.01
-    update_net_latent_penalty=0.01, # 0.01
-    choice_net_latent_penalty=0.001, # 0.001
-    l2_scale=1e-5)
-
-# Define a config for warmup training with no noise and no penalties
-disrnn_config_warmup = copy.deepcopy(disrnn_config)
-disrnn_config_warmup.latent_penalty = 0
-disrnn_config_warmup.choice_net_latent_penalty = 0
-disrnn_config_warmup.update_net_obs_penalty = 0
-disrnn_config_warmup.update_net_latent_penalty = 0
-disrnn_config_warmup.l2_scale = 0
-disrnn_config_warmup.noiseless_mode = True
-
-# Define network builder functions
-make_disrnn = lambda: disrnn.HkDisentangledRNN(disrnn_config)
-make_disrnn_warmup = lambda: disrnn.HkDisentangledRNN(disrnn_config_warmup)
-
-# Define an optimizer
-opt = optax.adam(learning_rate=0.001)
-
-# Warmup training with no noise and no penalties
-params, _, _ = rnn_utils.train_network(
-    make_disrnn_warmup,
-    training_dataset=trainDataset,
-    validation_dataset=testDataset,
-    loss="penalized_categorical",
-    params=None,
-    opt_state=None,
-    opt=opt,
-    n_steps=1000,
-    do_plot=True)
-
-# Additional training using information penalty
-params, _, _ = rnn_utils.train_network(
-    make_disrnn,
-    training_dataset=trainDataset,
-    validation_dataset=testDataset,
-    loss="penalized_categorical",
-    params=params,
-    opt_state=None,
-    opt=opt,
-    n_steps=8000,
-    do_plot=True)
-
-
+params = modelParams[0]
+disrnn_config = configs[0]
 # Plot bottleneck structure and update rules
 plotting.plot_bottlenecks(params, disrnn_config)
-plotting.plot_update_rules(params, disrnn_config)
+# plotting.plot_choice_rule(params, disrnn_config)
+# plotting.plot_update_rules(params, disrnn_config)
 
 # Eval disRNN on unseen data #
 # Use the wamrup disrnn, so that there will be no noise
